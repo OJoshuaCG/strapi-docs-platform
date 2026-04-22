a8748268346973e7f66c006a8c95afc2d8b64bbc
Joshua Carrasco <ojoshuacg@gmail.com>
2026-04-22 15:01:05 -0600

    del. Global settings. Fueron definidad por "spacio de la organizacion"

--- backend/cms/src/api/global-setting/controllers/global-setting.ts
+++
@@ -1,6 +0,0 @@
-import { factories } from '@strapi/strapi';
-
-// Use factory without type checking since content type is registered at runtime
-export default factories.createCoreController(
-  'api::global-setting.global-setting' as any
-);
--- backend/cms/src/api/global-setting/routes/global-setting.ts
+++
@@ -1,5 +0,0 @@
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter(
-  'api::global-setting.global-setting' as any
-);
